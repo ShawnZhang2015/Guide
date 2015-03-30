418988fec4d6be49cffd8adfb21b3636320a1415
ShawnZhang2015 <6346289@qq.com>
2015-03-30 15:45:06 +0800

    测试步骤事件函数

--- src/guideConfig.js
+++ src/guideConfig.js
@@ -11,7 +11,36 @@ var guideConfig = {
             {
                 log: "关闭第一盏灯",
                 command: sz.GuideCommand.GC_FINGER_HINT,
-                locator:"_fire1"
+                locator:"_fire1",
+
+                onEnter: function(cb) {
+                    var label = new cc.LabelTTF("关闭第一盏灯", "宋体", 48);
+                    var pt = cc.p(this.width / 2, this.height / 2);
+                    label.setPosition(pt);
+                    this.addChild(label);
+
+                    var faceOut = cc.fadeOut(3);
+                    var call = cc.callFunc(function() {
+                        label.removeFromParent();
+                        cb();
+                    }, this);
+                    label.runAction(cc.sequence(faceOut, call))
+                },
+
+                //onLocateNode: function(node) {
+                //    var point = node.getParent().convertToWorldSpace(node.getPosition());
+                //    point.x += node.width / 2;
+                //    point.y += node.height / 2;
+                //    this._fingerToPoint(point, true);
+                //}
+
+                onLocateNode: function(node) {
+                    this._touchRect.x -= node.width / 2;
+                    this._touchRect.y -= node.height / 2;
+                    var point = cc.p(this._touchRect.x + node.width / 2, this._touchRect.y + node.height / 2);
+                    this.showMask();
+                    this._fingerToPoint(point, true);
+                }
             },
 
             {
